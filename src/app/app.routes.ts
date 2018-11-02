d894fd36716f7863151b1728ff6d8eef3e23d031
marcosjrs <marcosjrs80@gmail.com>
2018-11-02 13:07:13 +0100

    Subruteado simple, sin recoger parametros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,25 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { NuevoUsuarioComponent } from './components/usuario/nuevo-usuario.component';
+import { EditarUsuarioComponent } from './components/usuario/editar-usuario.component';
+import { DetalleUsuarioComponent } from './components/usuario/detalle-usuario.component';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'usuario', component: UsuarioComponent },
+
+    {
+        path: 'usuario',
+        component: UsuarioComponent,
+        children: [
+            //Se podría llevar este array (const rutasUsuario:Routes = ....) a otro archivo dentro de carpeta "usuario", por ejemplo, importarlo y add aquí.
+            { path: 'nuevo', component: NuevoUsuarioComponent },
+            { path: 'editar', component: EditarUsuarioComponent },
+            { path: 'detalle', component: DetalleUsuarioComponent },
+            { path: '**', redirectTo: 'nuevo' }
+        ]
+    },
+
     { path: '**', redirectTo: 'home' }
 ];
 

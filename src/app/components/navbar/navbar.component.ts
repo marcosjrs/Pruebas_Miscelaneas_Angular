c8c34811ae02335dcc05842b25fa33d4222ae87c
marcosjrs <marcosjrs80@gmail.com>
2018-11-02 12:03:12 +0100

    Estilo de menu seleccionado

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: [`a{color:white}  .navbar-toggler{ outline-color: white} li{ margin-right:8px;}`]
+  styles: [`a{color:white}  .navbar-toggler{ outline-color: white} li{ margin-right:8px;} a.active{ font-weight: bold; }`]
 })
 export class NavbarComponent implements OnInit {
 

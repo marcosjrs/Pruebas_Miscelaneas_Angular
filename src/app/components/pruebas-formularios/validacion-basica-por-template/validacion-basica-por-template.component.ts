342e4bb2242b4daa9a77a33d85524767c73c7ec6
marcosjrs <marcosjrs80@gmail.com>
2018-11-03 13:22:37 +0100

    validación básica por template

--- src/app/components/pruebas-formularios/validacion-basica-por-template/validacion-basica-por-template.component.ts
+++ src/app/components/pruebas-formularios/validacion-basica-por-template/validacion-basica-por-template.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'validacion-basica-por-template',
   templateUrl: './validacion-basica-por-template.component.html'
 })
 export class ValidacionBasicaPorTemplateComponent implements OnInit {
+  usuario = {
+    nombre:"marcos j",
+    apellidos:"rs",
+    email:"marcosjrs80@gmail.com"
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  guardar(formulario:NgForm){
+    console.log(formulario);
   }
 
 }

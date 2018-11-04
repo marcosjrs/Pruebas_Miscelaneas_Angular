201331ed910b57063f31a9e22cf4f6ebef275cf4
marcosjrs <marcosjrs80@gmail.com>
2018-11-04 12:55:58 +0100

    Estableciendo valores en un FormGroup de forma "automática"

--- src/app/components/pruebas-formularios/validacion-basica-por-codigo/validacion-basica-por-codigo.component.ts
+++ src/app/components/pruebas-formularios/validacion-basica-por-codigo/validacion-basica-por-codigo.component.ts
@@ -8,15 +8,35 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class ValidacionBasicaPorCodigoComponent implements OnInit {
   formulario:FormGroup; //recordar importar ReactiveFormsModule en el app.module
 
+  usuario = {
+    nombre:"marcos j",
+    apellidos:"rs",
+    email:"marcosjrs80@gmail.com"
+  }
+
   constructor() {
+
     this.formulario = new FormGroup({
-      nombre:new FormControl("marcos j", Validators.required),
-      apellidos:new FormControl("rs", Validators.minLength(2) ),
-      email:new FormControl("marcosjrs80@gmail.com",[
+      nombre:new FormControl("", Validators.required),
+      apellidos:new FormControl(" ", Validators.minLength(2) ),
+      email:new FormControl("",[
                               Validators.required, 
                               Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}")
                             ]),
     });
+    this.formulario.setValue(this.usuario);
+    
+    /*
+    Si la estructura fuera 
+    usuario = {
+      nombreCompleto:{
+        nombre:"",
+        apellidos:"",
+      },
+      email:"marcosjrs80@gmail.com"
+    }
+    nombreCompleto tendría que ser un FormGroup y en el html debería englobarse los dos formControlName de nombre y apellidos en un div con formGroupName="nombreCompleto"
+    */
    }
 
   ngOnInit() {

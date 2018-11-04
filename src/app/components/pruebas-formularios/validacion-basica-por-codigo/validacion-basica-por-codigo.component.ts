74364ce2df18babe50c44380d2ac717383ed470f
marcosjrs <marcosjrs80@gmail.com>
2018-11-04 13:09:19 +0100

    Agrupando en FormGroup para cumplir con estructura de datos determinada

--- src/app/components/pruebas-formularios/validacion-basica-por-codigo/validacion-basica-por-codigo.component.ts
+++ src/app/components/pruebas-formularios/validacion-basica-por-codigo/validacion-basica-por-codigo.component.ts
@@ -7,36 +7,29 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class ValidacionBasicaPorCodigoComponent implements OnInit {
   formulario:FormGroup; //recordar importar ReactiveFormsModule en el app.module
-
+  
   usuario = {
-    nombre:"marcos j",
-    apellidos:"rs",
+    nombreCompleto:{
+      nombre:"marcos j",
+      apellidos:"rs"
+    },    
     email:"marcosjrs80@gmail.com"
   }
-
   constructor() {
 
     this.formulario = new FormGroup({
-      nombre:new FormControl("", Validators.required),
-      apellidos:new FormControl(" ", Validators.minLength(2) ),
+
+      nombreCompleto:new FormGroup({
+        nombre:new FormControl("", Validators.required),
+        apellidos:new FormControl(" ", Validators.minLength(2) ),
+      }),  
+
       email:new FormControl("",[
                               Validators.required, 
                               Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}")
                             ]),
     });
     this.formulario.setValue(this.usuario);
-    
-    /*
-    Si la estructura fuera 
-    usuario = {
-      nombreCompleto:{
-        nombre:"",
-        apellidos:"",
-      },
-      email:"marcosjrs80@gmail.com"
-    }
-    nombreCompleto tendría que ser un FormGroup y en el html debería englobarse los dos formControlName de nombre y apellidos en un div con formGroupName="nombreCompleto"
-    */
    }
 
   ngOnInit() {
@@ -44,6 +37,8 @@ export class ValidacionBasicaPorCodigoComponent implements OnInit {
 
   guardar(){
     console.log(this.formulario);
+    //this.formulario.reset();//vaciaría los valores, saltando las validaciones
+    this.formulario.reset(this.usuario);// lo rellenaría con los valores iniciales, quedando tambien pristine.
   }
 
 }
